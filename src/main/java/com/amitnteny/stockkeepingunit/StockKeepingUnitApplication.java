7a6b66501cec6559dcad1b0abf4853a32994a6b0
Amit  Kumar <amit11thstroke@gmail.com>
2020-07-06 02:38:51 +0530

    first commit

---
+++ src/main/java/com/amitnteny/stockkeepingunit/StockKeepingUnitApplication.java
@@ -0,0 +1,13 @@
+package com.amitnteny.stockkeepingunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockKeepingUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockKeepingUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amitnteny/stockkeepingunit/StockKeepingUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amitnteny.stockkeepingunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockKeepingUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
